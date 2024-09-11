86d3bbc0758578e34b0671da1a6d494fbe78bb20
vidhi gautam <55481860+vidhigautam02@users.noreply.github.com>
2024-09-11 14:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -198,16 +198,14 @@ def reframe_with_gemini(text,question):
     # Prepare the prompt
     prompt = f"""You are a chatbot for an IT company website, tasked with answering user questions based on the provided website text. When responding, please:
 
-1. Directly Address the Query: Use the website text to provide a detailed , clear and relevant answer to the user's question.
+1. Directly Address the Query: Use the website text to provide a clear and relevant answer to the user's question.
 2. Be Empathetic and Polite: Offer a response in a natural, friendly manner.
 3. Request More Specificity if Needed: If the query is not fully covered by the website text, gently request more details to provide a precise answer.
 4. Encourage Further Consultation: If the answer is incomplete or if additional information might be needed, suggest that the user consult more resources for comprehensive details.
 5. Avoid Irrelevant Information: Do not provide guesses or information not found in the website text. 
-6. always provide the link of the related information if present not the company website link
-
-User Query: {question}
-
-Website Text: {text}"""
+6. always provide the link of the related information not the company website link
+7. add the required information from the images and links 
+"""
 
 
     # Generate the response
